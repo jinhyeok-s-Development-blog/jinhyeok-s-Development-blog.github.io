a784330b29fdd2148830a8bff99fd7db942df97b
wonderfulrosemari <whwlsgur1419@naver.com>
2024-03-18 10:01:04 +0900

    블로그 대문 변경

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -103,7 +103,7 @@ const config = {
 
           { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/wonderfulrosemari",
             label: "GitHub",
             position: "right",
           },
@@ -113,36 +113,11 @@ const config = {
         style: "dark",
         links: [
           {
-            title: "Docs",
-            items: [],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
-          {
             title: "More",
             items: [
               {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/wonderfulrosemari",
               },
             ],
           },
